267192203047de637e6f74be221ce32f7cc05316
odiegomq <marquesd062@gmail.com>
2023-03-12 15:26:42 -0300

    configuração das rotas

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// create express app
+const app = express();
+// Setup server port
+const port = process.env.PORT || 5000;
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+// define a root route
+app.get('/create', (req, res) => {
+    res.send("Hello World");
+});
+app.get('/delete', (req, res) => {
+    res.send("Hello World");
+});
+app.get('/edit', (req, res) => {
+    res.send("Hello World");
+});
+app.get('/get?id=', (req, res) => {
+    res.send("Hello World");
+});
+app.get('/getAll', (req, res) => {
+    res.send("Hello World");
+});
+// listen for requests
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
